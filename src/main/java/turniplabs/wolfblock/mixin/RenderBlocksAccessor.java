e5d2897dcbeb8f14569b60332f90a655e74c0de4
azurelmao <de_arcane@protonmail.com>
2023-08-20 10:55:17 +0200

    Remap

--- src/main/java/turniplabs/wolfblock/mixin/RenderBlocksAccessor.java
+++ src/main/java/turniplabs/wolfblock/mixin/RenderBlocksAccessor.java
@@ -4,7 +4,7 @@ import net.minecraft.client.render.RenderBlocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(RenderBlocks.class)
+@Mixin(value = RenderBlocks.class, remap = false)
 public interface RenderBlocksAccessor {
     @Accessor
     void setOverrideBlockTexture(int overrideBlockTexture);
